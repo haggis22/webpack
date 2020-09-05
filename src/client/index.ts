c3cce42316b264ccfb1529de85d215e75295359e
haggis22 <dshell@gmail.com>
2020-09-05 17:59:07 -0500

    Vue now running as Typescript file

---
+++ src/client/index.ts
@@ -0,0 +1,20 @@
+﻿import './style.scss';
+
+import Vue from 'vue';
+
+var app = new Vue({
+    el: '#poker',
+    data: {
+        name: 'Daniel',
+        message: 'Here we go again',
+        players: 
+            [
+                { name: 'Daniel', chips: 500, playing: false },
+                { name: 'Mark', chips: 200, playing: false }
+            ]
+
+
+    }
+});
+
+console.log('Here I am');
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,54 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+// const webpack = require('webpack');
 
 module.exports = {
 
-    entry: { index: path.resolve(__dirname, "src/client/poker", "poker.js") },
-    output: { path: path.resolve(__dirname, "dist/poker") },
+    entry: { index: path.resolve(__dirname, "./src/client", "index.ts") },
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, "dist")
+    },
 
     module: {
         rules: [
 
+/*
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+                        // the "scss" and "sass" values for the lang attribute to the right configs here.
+                        // other preprocessors should work out of the box, no loader config like this necessary.
+                        'scss': 'vue-style-loader!css-loader!sass-loader',
+                        'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+                    }
+                    // other vue-loader options go here
+                }
+            },
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader"]
+                use: ["vue-style-loader", "css-loader"]
             },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+*/
             {
                 test: /\.scss$/,
                 use: ["style-loader", "css-loader", 'sass-loader']
+            },
+            {
+                test: /\.tsx?$/,
+                use: ["ts-loader"],
+                exclude: /node_modules/
             }
 
         ]
@@ -27,13 +60,17 @@ module.exports = {
 
     plugins: [
 
-        new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src/client/poker", "index.html")})
+        new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src/client", "index.html") })
+
     ],
 
     resolve: {
+        // extensions: ['.ts', '.js', '.vue', '.json'],
+        extensions: ['.ts' ],
         alias: {
-            vue: 'vue/dist/vue.js'
+            'vue$': 'vue/dist/vue.esm.js'
         }
     }
+
 
 };
